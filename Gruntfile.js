08cf655f6902f813f5b8d6890ae28dffc7831024
reg4in <gunzof@gmx.de>
2014-05-05 22:11:18 +0200

    fix: replace tabs with spaces

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,31 +1,31 @@
 module.exports = function(grunt) {
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
-		stylus: {
-			i: {
-				files: {
-					'build/i.min.css': [
-						'styl/*.styl'
-					]
-				}
-			}
-		},
-		watch: {
-			stylus: {
-				files: [
-					'styl/*.styl'
-				],
-				tasks: [
-					'stylus'
-				]
-			}
-		}
-	});
-	
-	grunt.loadNpmTasks('grunt-contrib-stylus');
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	
-	grunt.registerTask('default', [
-		'stylus'
-	]);
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    stylus: {
+      i: {
+        files: {
+         'build/i.min.css': [
+            'styl/*.styl'
+          ]
+        }
+      }
+    },
+    watch: {
+      stylus: {
+        files: [
+         'styl/*.styl'
+        ],
+        tasks: [
+         'stylus'
+        ]
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-stylus');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', [
+   'stylus'
+  ]);
 };
